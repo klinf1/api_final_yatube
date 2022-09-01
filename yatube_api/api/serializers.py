319c8f963dfd9dda6ef5beb1fdc50789aea5a039
klinf <klinf157@gmail.com>
2022-09-01 21:47:16 +0300

    validdate_followinng

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -68,9 +68,9 @@ class FollowSerializer(serializers.ModelSerializer):
             )
         ]
 
-    def validate(self, data):
-        if data['user'] == data['following']:
+    def validate_following(self, value):
+        if self.context.get('request').user == value:
             raise serializers.ValidationError(
                 'Нельзя подписаться на самого себя!'
             )
-        return data
+        return value
