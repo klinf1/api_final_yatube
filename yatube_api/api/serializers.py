b10495d2fab3bef6e82d30dad170d0d84df28c95
Михаил Манузин <klinf157@gmail.com>
2022-09-02 14:15:22 +0300

    done docstrings

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,12 @@
-import base64
-
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 from django.contrib.auth import get_user_model
-from django.core.files.base import ContentFile
 
 from posts.models import Comment, Post, Group, Follow
+from .custom_fields import Base64ImageField
 
 
 User = get_user_model()
-
-
-class Base64ImageField(serializers.ImageField):
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-        return super().to_internal_value(data)
 
 
 class PostSerializer(serializers.ModelSerializer):
